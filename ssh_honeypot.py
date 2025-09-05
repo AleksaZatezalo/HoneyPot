5c4cf87fafc5f133a5bb0d9cdc9be9a81e892e4b
Aleksa Zatezalo <zabumaphu@gmail.com>
2025-09-06 00:11:27 +0200

    Paramiko finished

--- ssh_honeypot.py
+++ ssh_honeypot.py
@@ -7,6 +7,7 @@ Author: Aleksa Zatezalo
 import logging
 from logging.handlers import RotatingFileHandler
 import socket
+import paramiko
 
 # Constants
 logging_format = logging.Formatter('%(message)s')
@@ -55,7 +56,37 @@ def emulated_shell(channel, client_ip):
         channel.send(b'corporate-jumpbox2$ ')
         command = b""
             
-
 # SSH Server + Socket
+class Server(paramiko.ServerInterface):
+
+    def __init__(self, client_ip, input_username=None, input_password=None):
+        self.client_ip = client_ip
+        self.input_username = input_username
+        self.input_password = input_password
+
+    def check_channel_request(self, kind: str, chanid: int) -> int:
+        if kind == 'session':
+            return paramiko.OPEN_SUCCEEDED
+    
+    def get_allowed_auths(self):
+        return "password"
+    
+    def check_auth_password(self, username, password):
+        if self.input_username is not None and self.input_password is not None:
+            if username == 'username' and password == 'password':
+                return paramiko.AUTH_SUCCESSFUL
+            else:
+                return paramiko.AUTH_FAILED
+            
+    def check_channel_shell_request(self, channel):
+        self.event.set()
+        return True
+    
+    def check_channel_pty_request(self, channel, term, width, height, pixelwidth, pixelheight, modes):
+        return True
+    
+    def check_channel_exec_request(self, channel, command):
+        command = str(command)
+        return True
 
 # Provisioning SSH Based Honeypot
